08155afe9015ababc7d55582c800e0237572e8b7
fardog <nate@milkandtang.com>
2015-01-15 22:20:07 -0800

    Prep for launch.

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 Add or remove trailing slashes, and redirect.
 
-**Work in progress!** Please check back in a few days.
+[![Build Status](http://img.shields.io/travis/fardog/trailing-slash/master.svg?style=flat)](https://travis-ci.org/fardog/trailing-slash)
+[![npm install](http://img.shields.io/npm/dm/trailing-slash.svg?style=flat)](https://www.npmjs.org/package/trailing-slash)
+
+
+`trailing-slash` allows you to wrap a [http.Server][server] handler function, and
+redirect when trailing slashes don't meet your expectations.
 
 ## Example
 
@@ -15,14 +20,37 @@ var trailing = trailingSlash(true, done)
 trailing(req, res) // redirects to '/post/some-title/'
 
 function done(req, res) {
-  // called after successful redirect
+  // would be called after successful redirect
+  console.log(req.url) // '/post/some-title/'
 }
 ```
 
 ## API
 
-Coming soon...
+- `trailingSlash(shouldHaveSlash, next [, statusCode])` - Create a new handler,
+  which returns a function. Accepts the following parameters:
+    - `shouldHaveSlash` - A boolean, representing whether or not the route
+      should have a slash at the end; `true` for yes, `false` for no.
+    - `next` - The function to be called if the slashes match expectations, and
+      a redirect is not necessary.
+    - `statusCode` - An integer for the status code that should be sent--either
+      301 (permanent redirect) or 302 (temporary redirect)--if a redirect is
+      necessary. Defaults to 301 if this parameter is not provided.
+
+Returns a function with the following signature:
+
+- `trailing(request, response [, args ...])`
+    - `request` - An [http.IncomingMessage][request], as passed by an
+      [http.Server][server] or similar.
+    - `response` - An [http.ServerResponse][response], as passed by an
+      [http.Server][server] or similar.
+    - `args` - Any number of arguments which will be passed to the matched
+      function.
 
 ## License
 
 MIT. See [LICENSE](./LICENSE) for details.
+
+[request]: http://nodejs.org/api/http.html#http_http_incomingmessage
+[server]: http://nodejs.org/api/http.html#http_class_http_server
+[routes]: https://www.npmjs.com/package/routes 
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var test = require('tape')
   , trailing = require('./index')
 
-test('does not redirect when slashes match', function(t) {
+test('does not redirect when slashes requested and match', function(t) {
   t.plan(1)
 
   var redirect = trailing(true, done)
@@ -17,7 +17,7 @@ test('does not redirect when slashes match', function(t) {
   }
 })
 
-test('does not redirect when no slashes match', function(t) {
+test('does not redirect when no slashes requested and match', function(t) {
   t.plan(1)
 
   var redirect = trailing(false, done)
@@ -36,12 +36,12 @@ test('does not redirect when no slashes match', function(t) {
 test('redirects when slash is missing', function(t) {
   t.plan(3)
 
-  var redirect = trailing(true, done)
+  var redirect = trailing(true, fail)
     , req = {url: '/something/else'}
     , res = {
           setHeader: function(name, value) {
             t.equal(name, 'Location')
-            t.equal(value, req.url + '/')
+            t.equal(value, '/something/else/')
           }
         , end: function() {
             t.ok(true)
@@ -51,7 +51,7 @@ test('redirects when slash is missing', function(t) {
 
   redirect(req, res)
 
-  function done(request, response) {
+  function fail(request, response) {
     t.ok(false) // fail if we reach here
   }
 })
@@ -59,12 +59,12 @@ test('redirects when slash is missing', function(t) {
 test('redirects when slash is present', function(t) {
   t.plan(3)
 
-  var redirect = trailing(false, done)
+  var redirect = trailing(false, fail)
     , req = {url: '/something/else/'}
     , res = {
           setHeader: function(name, value) {
             t.equal(name, 'Location')
-            t.equal(value, req.url.slice(0, -1))
+            t.equal(value, '/something/else')
           }
         , end: function() {
             t.ok(true)
@@ -74,7 +74,96 @@ test('redirects when slash is present', function(t) {
 
   redirect(req, res)
 
-  function done(request, response) {
+  function fail(request, response) {
+    t.ok(false) // fail if we reach here
+  }
+})
+
+test('retains other URL parts', function(t) {
+  t.plan(2)
+
+  var redirect = trailing(false, fail)
+    , req = {url: '/something/else/?woohoo=true'}
+    , res = {
+          setHeader: function(name, value) {
+            t.equal(value, '/something/else?woohoo=true')
+          }
+        , end: function() {
+            t.ok(true)
+            t.end()
+        }
+      }
+
+  redirect(req, res)
+
+  function fail(request, response) {
+    t.ok(false) // fail if we reach here
+  }
+})
+
+test('passes through any number of objects', function(t) {
+  t.plan(3)
+
+  var redirect = trailing(false, done)
+    , req = {url: '/something/else'}
+    , res = {}
+    , superflous = {}
+
+  redirect(req, res, superflous)
+
+  function done(request, response, unnecessary) {
+    t.strictEqual(request, req)
+    t.strictEqual(response, res)
+    t.strictEqual(superflous, unnecessary)
+
+    t.end()
+  }
+})
+
+test('defaults to a 301 redirect', function(t) {
+  t.plan(2)
+
+  var redirect = trailing(false, fail)
+    , req = {url: '/something/else/?woohoo=true'}
+    , res = {
+          setHeader: function(name, value) {
+            t.equal(value, '/something/else?woohoo=true')
+          }
+        , end: success
+      }
+
+  redirect(req, res)
+
+  function success() {
+    t.equal(res.statusCode, 301)
+    t.end()
+  }
+
+  function fail(request, response) {
+    t.ok(false) // fail if we reach here
+  }
+})
+
+test('performs a 302 redirect when requested', function(t) {
+  t.plan(2)
+
+  var redirect = trailing(false, fail, 302)
+    , req = {url: '/something/else/?woohoo=true'}
+    , res = {
+          setHeader: function(name, value) {
+            t.equal(value, '/something/else?woohoo=true')
+          }
+        , end: success
+      }
+
+  redirect(req, res)
+
+  function success() {
+    t.equal(res.statusCode, 302)
+    t.end()
+  }
+
+  function fail(request, response) {
     t.ok(false) // fail if we reach here
   }
 })
